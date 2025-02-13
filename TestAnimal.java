1e32ce4bddc54af7f8a508b786962cad1ce0e453
Stephen Badcock <badcockstephen73@gmail.com>
2025-02-13 14:20:22 -0330

    added mammal subclass

--- TestAnimal.java
+++ TestAnimal.java
@@ -1,7 +1,11 @@
 public class TestAnimal {
     public static void main(String[] args) {
+        /*
         Animal genericAnimal = new Animal("Creature");
-
         System.out.println(genericAnimal);
+        */
+
+        Mammal genericMammal = new Mammal("Primate");
+        System.out.println(genericMammal);
     }
 }

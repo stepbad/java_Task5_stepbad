704e16a143aa214c8030d2276fc1999ea0ea9f18
Stephen Badcock <badcockstephen73@gmail.com>
2025-02-13 14:06:35 -0330

    revised animal to fit uml

--- Animal.java
+++ Animal.java
@@ -5,15 +5,7 @@ public class Animal {
         this.name = name;
     }
 
-    public void makeSound() {
-        System.out.println("Rooooooaaaaaaaar!!!!!!");
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public String toString() {
-        return "Animal: " + name;
+        return "Animal[name=\"" + name + "\"]";
     }
 }
